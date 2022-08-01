57d23e7567f33b0f184c2ce244c32daa7d0a507c
putrya1969 <putrya1969@gmail.com>
2022-08-01 23:22:35 +0300

    check error of calculation

--- BeetrootLesson2/Program.cs
+++ BeetrootLesson2/Program.cs
@@ -1,6 +1,6 @@
 ﻿int firstNum = 3;
 int secondNum = 5;
-Console.WriteLine($"Calculation result: {-6 * firstNum ^ 3 + 5 * firstNum ^ 2 - 10 * firstNum + 15}");
+Console.WriteLine($"Calculation result: {-6 * Math.Pow(firstNum, 3) + 5 * Math.Pow(firstNum, 2) - 10 * firstNum + 15}");
 Console.WriteLine($"Calculation result: {Math.Abs(firstNum) * Math.Sin(firstNum)}");
 Console.WriteLine($"Calculation result: {2 * Math.PI * firstNum}");
 Console.WriteLine($"Calculation result: {Math.Max(firstNum, secondNum)}");
