c50008f3d38d0a816a7a8cbbf6802ee902e8d705
putrya1969 <putrya1969@gmail.com>
2022-08-01 23:12:17 +0300

    check error of calculation

--- BeetrootLesson2/Program.cs
+++ BeetrootLesson2/Program.cs
@@ -1,4 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-int variable;
-variable = 5;
-Console.WriteLine(variable);
+﻿int firstNum = 3;
+int secondNum = 5;
+Console.WriteLine($"Calculation result: {-6 * firstNum ^ 3 + 5 * firstNum ^ 2 - 10 * firstNum + 15}");
+Console.WriteLine($"Calculation result: {Math.Abs(firstNum) * Math.Sin(firstNum)}");
+Console.WriteLine($"Calculation result: {2 * Math.PI * firstNum}");
+Console.WriteLine($"Calculation result: {Math.Max(firstNum, secondNum)}");
+Console.ReadKey();
